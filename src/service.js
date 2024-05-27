1ad36b24fea048c56591550a72ea439faa75a222
vlvch <107064521+vlvch@users.noreply.github.com>
2024-05-27 18:52:30 +0300

    fix: remove timeout

--- src/service.js
+++ src/service.js
@@ -4,8 +4,8 @@ const getCountries = async () => {
   const response = await axios.get('https://restcountries.com/v3.1/all')
     .then((response) => response.data)
     .catch((e) => {
-      console.log(e.message, 'new try');
-      return setTimeout(getCountries, 1000);
+      console.log(e.message, ', new try');
+      return getCountries();
     })
   return response;
 }
@@ -14,8 +14,8 @@ const getCurrentCountry = async (name) => {
   const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`)
     .then((response) => response.data)
     .catch((e) => {
-      console.log(e.message, 'new try')
-      return setTimeout(getCurrentCountry(name), 1000);
+      console.log(e.message, ', new try')
+      return getCurrentCountry(name);
     })
   return response;
 }
